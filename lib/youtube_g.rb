0fc9391cff95daf591be478731554e45976dfc0d
Daniel Insley <dan@viviti.com>
2008-07-07 10:48:44 -0700

    updated todo list

--- lib/youtube_g.rb
+++ lib/youtube_g.rb
@@ -1,18 +1,12 @@
-require File.dirname(__FILE__) + '/youtube_g/client'
-require File.dirname(__FILE__) + '/youtube_g/record'
-require File.dirname(__FILE__) + '/youtube_g/parser'
-require File.dirname(__FILE__) + '/youtube_g/model/author'
-require File.dirname(__FILE__) + '/youtube_g/model/category'
-require File.dirname(__FILE__) + '/youtube_g/model/contact'
-require File.dirname(__FILE__) + '/youtube_g/model/content'
-require File.dirname(__FILE__) + '/youtube_g/model/playlist'
-require File.dirname(__FILE__) + '/youtube_g/model/rating'
-require File.dirname(__FILE__) + '/youtube_g/model/thumbnail'
-require File.dirname(__FILE__) + '/youtube_g/model/user'
-require File.dirname(__FILE__) + '/youtube_g/model/video'
-require File.dirname(__FILE__) + '/youtube_g/request/video_upload'
-require File.dirname(__FILE__) + '/youtube_g/request/video_search'
-require File.dirname(__FILE__) + '/youtube_g/response/video_search'
+require 'logger'
+require 'open-uri'
+require 'net/https'
+require 'digest/md5'
+require 'rexml/document'
+require 'cgi'
+
+ruby_files = Dir.glob( File.join(File.dirname(__FILE__), "/youtube_g/**", "*.rb") )
+ruby_files.each { |file| require file }
 
 class YouTubeG #:nodoc:
   VERSION = '0.4.5'
