d7c871c0f720d1bc8604f8a4545ea5353d142ed1
Layton Wedgeworth <laytonw@layton.local>
2008-08-16 12:16:09 -0700

    Add pagination to UserSearch

--- lib/youtube_g/request/user_search.rb
+++ lib/youtube_g/request/user_search.rb
@@ -1,16 +1,36 @@
 class YouTubeG
   module Request #:nodoc:  
     class UserSearch < BaseSearch #:nodoc:      
+      attr_reader :max_results                     # max_results
+      attr_reader :order_by                        # orderby, ([relevance], viewCount, published, rating)
+      attr_reader :offset                          # start-index
+
       def initialize(params, options={})
         @url = base_url
-        return @url << "#{options[:user]}/favorites" if params == :favorites
-        @url << "#{params[:user]}/uploads" if params[:user]
+
+        if params == :favorites
+          @url << "#{options[:user]}/favorites" 
+        elsif params[:user]
+          @url << "#{params[:user]}/uploads"
+        end
+        
+        set_instance_variables(params)
+
+        @url << build_query_params(to_youtube_params)
       end
 
       private
 
       def base_url #:nodoc:
         super << "users/"
+      end
+
+      def to_youtube_params #:nodoc:
+        {
+          'max-results' => @max_results,
+          'orderby' => @order_by,
+          'start-index' => @offset
+        }
       end
     end
     
