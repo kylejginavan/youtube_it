b9417c5f9d5fec9ffa5e23506df214a2659e4353
Julik <me@julik.nl>
2009-02-25 08:03:48 +0100

    Attach the new parts, use Builder for XML packet gen

--- lib/youtube_g.rb
+++ lib/youtube_g.rb
@@ -4,6 +4,7 @@ require 'net/https'
 require 'digest/md5'
 require 'rexml/document'
 require 'cgi'
+require 'builder'
 
 require File.dirname(__FILE__) + '/youtube_g/client'
 require File.dirname(__FILE__) + '/youtube_g/record'
@@ -23,6 +24,7 @@ require File.dirname(__FILE__) + '/youtube_g/request/standard_search'
 require File.dirname(__FILE__) + '/youtube_g/request/video_upload'
 require File.dirname(__FILE__) + '/youtube_g/request/video_search'
 require File.dirname(__FILE__) + '/youtube_g/response/video_search'
+require File.dirname(__FILE__) + '/youtube_g/chain_io'
 
 class YouTubeG #:nodoc:
   VERSION = '0.5.0'
