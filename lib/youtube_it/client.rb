5b1b1c5051d0723c8be86d1f277702e1ca2ef068
mauro <maurotorres@gmail.com>
2010-11-26 20:55:25 -0300

    support for authsub auth

--- lib/youtube_it/client.rb
+++ lib/youtube_it/client.rb
@@ -196,9 +196,54 @@ class YouTubeIt
       end
     end
 
+    def create_session_token
+      response = nil
+      session_token_url = "/accounts/AuthSubSessionToken"
+
+      http_connection do |session|
+        response = session.get2('https://%s' % session_token_url,session_token_header).body
+      end
+      @authsub_token = response.sub('Token=','')
+    end
+
+    def revoke_session_token
+      response = nil
+      session_token_url = "/accounts/AuthSubRevokeToken"
+
+      http_connection do |session|
+        response = session.get2('https://%s' % session_token_url,session_token_header).code
+      end
+      response.to_s == '200' ? true : false
+    end
+
+    def session_token_info
+      response = nil
+      session_token_url = "/accounts/AuthSubTokenInfo"
+
+      http_connection do |session|
+        response = session.get2('https://%s' % session_token_url,session_token_header)
+      end
+      {:code => response.code, :body => response.body }
+    end
+    
     private
       def client
         @client ||= YouTubeIt::Upload::VideoUpload.new(:dev_key => @dev_key, :authsub_token => @authsub_token)
+      end
+      
+      def session_token_header
+        {
+          "Content-Type"   => "application/x-www-form-urlencoded",
+          "Authorization"  => "AuthSub token=#{@authsub_token}"
+        }        
+      end
+      
+      def http_connection
+        http = Net::HTTP.new("www.google.com")
+        http.set_debug_output(logger) if @http_debugging
+        http.start do |session|
+          yield(session)
+        end
       end
   end
 
