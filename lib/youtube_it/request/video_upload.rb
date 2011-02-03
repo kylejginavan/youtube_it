461fd6134ebb81bc5a8eb0d9e1efa7b61def5c0a
Kyle J. Ginavan <kylejginavan@gmail.com>
2011-02-02 20:10:00 -0600

    google doesn't use 401 or 403 for oauth fail

--- lib/youtube_it/request/video_upload.rb
+++ lib/youtube_it/request/video_upload.rb
@@ -474,7 +474,8 @@ puts "========= response body #{response.to_yaml}"
 
       def raise_on_faulty_response(response)
         response_code = response.code.to_i
-        if response_code == 403 || response_code == 401
+        #if response_code == 403 || response_code == 401
+        if response_code / 10 == 40
           raise AuthenticationError, response.body[/<TITLE>(.+)<\/TITLE>/, 1]
         elsif response_code / 10 != 20 # Response in 20x means success
           raise UploadError, parse_upload_error_from(response.body.gsub(/\n/,''))
