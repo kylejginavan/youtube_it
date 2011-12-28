ef0f290fd8c11aa2bba70534c8a988e41e420c22
chebyte <maurotorres@gmail.com>
2011-12-28 14:23:09 -0300

    taking nokogiri from depedencies and fix test

--- test/helper.rb
+++ test/helper.rb
@@ -3,7 +3,6 @@ require 'date'
 require 'test/unit'
 require 'pp'
 require 'open-uri'
-require 'nokogiri'
 require File.dirname(__FILE__) + '/../lib/youtube_it'
 
 YouTubeIt.logger.level = Logger::ERROR
