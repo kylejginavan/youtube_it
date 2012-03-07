9de94a71cea64c25cdbd603a4c51b4dbed8fbf5d
Kyle Fleming <kyle@rexly.com>
2012-03-06 20:20:46 -0800

    use the proper method for assertions

--- test/test_client.rb
+++ test/test_client.rb
@@ -279,7 +279,7 @@ class TestClient < Test::Unit::TestCase
     
   def test_should_list_playlist_for_user
     result = @client.playlists('chebyte')
-    assert result.last.title, "rock"
+    assert_equal "rock", result.last.title
   end
     
   def test_should_determine_if_widescreen_video_is_widescreen
@@ -360,7 +360,7 @@ class TestClient < Test::Unit::TestCase
   
   def test_esc
     result = YouTubeIt.esc("спят усталые игрушки")
-    assert result, "спят+усталые+игрушки"
+    assert_equal "%D1%81%D0%BF%D1%8F%D1%82+%D1%83%D1%81%D1%82%D0%B0%D0%BB%D1%8B%D0%B5+%D0%B8%D0%B3%D1%80%D1%83%D1%88%D0%BA%D0%B8", result
   end
   
   def test_unicode_query
