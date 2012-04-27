eb93f4e3de22f9c1579786a4d79060406cacae66
H.J. Blok <hj.blok@gmail.com>
2012-04-27 15:20:12 +0200

    added orderby parameter for a playlist see https://google-developers.appspot.com/youtube/2.0/reference#orderbysp for possible orderby values

--- lib/youtube_it/model/playlist.rb
+++ lib/youtube_it/model/playlist.rb
@@ -2,8 +2,9 @@ class YouTubeIt
   module Model
     class Playlist < YouTubeIt::Record
       attr_reader :title, :description, :summary, :playlist_id, :xml, :published, :response_code
-      def videos
-        YouTubeIt::Parser::VideosFeedParser.new("http://gdata.youtube.com/feeds/api/playlists/#{playlist_id}?v=2").parse_videos
+      def videos(order_by = :position)
+        playlist_url = "/feeds/api/playlists/%s?v=2&orderby=%s" % [playlist_id, order_by]
+        YouTubeIt::Parser::VideosFeedParser.new("http://gdata.youtube.com/#{playlist_url}").parse_videos
       end
     end
   end
