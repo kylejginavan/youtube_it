97f2986dc60a4f573b51a5500028748c96a57be9
Mauro Torres <maurotorres@gmail.com>
2012-05-07 11:57:06 -0300

    fixing the method playlist.videos

--- lib/youtube_it/model/playlist.rb
+++ lib/youtube_it/model/playlist.rb
@@ -3,7 +3,7 @@ class YouTubeIt
     class Playlist < YouTubeIt::Record
       attr_reader :title, :description, :summary, :playlist_id, :xml, :published, :response_code
       def videos
-        YouTubeIt::Parser::VideosFeedParser.new(@xml).parse_videos
+        YouTubeIt::Parser::VideosFeedParser.new("http://gdata.youtube.com/feeds/api/playlists/#{playlist_id}?v=2").parse_videos
       end
     end
   end
