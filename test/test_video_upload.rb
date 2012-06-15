38939b8987db1b7121f7a7c7031414b1a802d464
Adrien Jarthon <adrien.jarthon@dimelo.com>
2012-06-15 17:30:55 +0200

    html error parsing fix

--- test/test_video_upload.rb
+++ test/test_video_upload.rb
@@ -17,4 +17,30 @@ class TestVideoUpload < Test::Unit::TestCase
 
     assert_equal "Service Unavailable: ServiceUnavailableException\n", @vu.send(:parse_upload_error_from, body)
   end
+
+  def test_should_extract_error_description_from_html
+    body = '<HTML>
+              <HEAD>
+                <TITLE>Token invalid - Invalid token: Stateless token expired</TITLE>
+              </HEAD>
+              <BODY BGCOLOR="#FFFFFF" TEXT="#000000">
+                <H1>Token invalid - Invalid token: Stateless token expired</H1>
+                <H2>Error 401</H2>
+              </BODY>
+            </HTML>'
+
+    assert_equal "Token invalid - Invalid token: Stateless token expired", @vu.send(:parse_upload_error_from, body)
+
+    body = '<HTML>
+              <HEAD>
+                <TITLE>NoLinkedYouTubeAccount</TITLE>
+              </HEAD>
+              <BODY BGCOLOR="#FFFFFF" TEXT="#000000">
+                <H1>NoLinkedYouTubeAccount</H1>
+                <H2>Error 401</H2>
+              </BODY>
+            </HTML>'
+
+    assert_equal "NoLinkedYouTubeAccount", @vu.send(:parse_upload_error_from, body)
+  end
 end
